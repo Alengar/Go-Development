d4bc282a394e9595cdad11761558067c35fca77e
Alengar <114170656+Alengar@users.noreply.github.com>
2023-09-15 13:09:06 +0600

    3rd commit

--- lecture1/task3/main_test.go
+++ lecture1/task3/main_test.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestSliceEqual(t *testing.T) {
+	tests := []struct {
+		slice1   []int
+		slice2   []int
+		expected bool
+	}{
+		{[]int{1, 2, 3}, []int{1, 2, 3}, true},
+		{[]int{1, 2, 3}, []int{3, 2, 1}, true},
+		{[]int{1, 2, 3}, []int{1, 2}, false},
+		{[]int{1, 2}, []int{1, 2, 3}, false},
+		{[]int{}, []int{}, true},
+	}
+
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			result := sliceEqual(test.slice1, test.slice2)
+			if result != test.expected {
+				t.Errorf("Expected %v, but got %v for %v and %v", test.expected, result, test.slice1, test.slice2)
+			}
+		})
+	}
+}
--- lecture1/task4/main_test.go
+++ lecture1/task4/main_test.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIntSliceSort(t *testing.T) {
+	tests := []struct {
+		input    IntSlice
+		expected IntSlice
+	}{
+		{IntSlice{9, 8, 9, 3, 11, 5}, IntSlice{3, 5, 8, 9, 9, 11}},
+		{IntSlice{5, 4, 3, 2, 1}, IntSlice{1, 2, 3, 4, 5}},
+		{IntSlice{1}, IntSlice{1}},
+		{IntSlice{}, IntSlice{}},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("Sorting %v", test.input), func(t *testing.T) {
+			test.input.Sort()
+			if !test.input.Equal(test.expected) {
+				t.Errorf("Expected %v, but got %v", test.expected, test.input)
+			}
+		})
+	}
+}
+
+func (s IntSlice) Equal(other IntSlice) bool {
+	if len(s) != len(other) {
+		return false
+	}
+	for i, val := range s {
+		if val != other[i] {
+			return false
+		}
+	}
+	return true
+}
